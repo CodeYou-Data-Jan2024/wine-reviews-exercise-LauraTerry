5fb19fa28943ac053c88a3ba9d3d596a5a320faf
Laura Terry <proxycharm@gmail.com>
2024-05-09 18:40:55 -0400

    updated comments

--- reviews.py
+++ reviews.py
@@ -1,19 +1,22 @@
 import pandas as pd
 
-reviews = pd.read_csv("data/winemag-data-130k-v2.csv/winemag-data-130k-v2.csv")
 #read in csv file
+reviews = pd.read_csv("data/winemag-data-130k-v2.csv/winemag-data-130k-v2.csv")
 
-mean_points = reviews.groupby('country')['points'].mean().apply(lambda point: round(point, 1))
 #discover mean points per country, adjust all points to round to .1 decimal point
+mean_points = reviews.groupby('country')['points'].mean().apply(lambda point: round(point, 1))
 
-countries_reviewed = reviews.value_counts('country')
 #discover counts of how many times a country was reviewed
+countries_reviewed = reviews.value_counts('country')
 
-new_df = [countries_reviewed, mean_points]
 #combine dfs into one new dataframe variable 
+new_df = [countries_reviewed, mean_points]
 
-results = pd.concat(new_df, axis = 1)
 #concat df with same axis as a guide
+results = pd.concat(new_df, axis = 1)
 
-results.to_csv("data/reviews-per-country.csv")
 #export results to new CSV file in data folder
+results.to_csv("data/reviews-per-country.csv")
+
+
+print(results)
